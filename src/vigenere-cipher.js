6d1bb02108e8dfa8013847d1680da769315383f1
Salohiddin98 <salohiddin.rashidov98@gmail.com>
2022-05-02 23:22:04 +0500

    feat:solve vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,5 +1,4 @@
 const { NotImplementedError } = require("../extensions/index.js");
-
 /**
  * Implement class VigenereCipheringMachine that allows us to create
  * direct and reverse ciphering machines according to task description
@@ -20,8 +19,65 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 class VigenereCipheringMachine {
-  encrypt() {}
-  decrypt() {}
+  constructor(bool = true) {
+    this.bool = bool;
+  }
+
+  encrypt(str, key) {
+    if (str === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    } else {
+      return this.action(str, key, true);
+    }
+  }
+  decrypt(str, key) {
+    if (str === undefined || key === undefined) {
+      throw new Error("Incorrect arguments!");
+    } else {
+      return this.action(str, key, false);
+    }
+  }
+  action(str, key, action) {
+    // prettier-ignore
+    let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+    // prettier-ignore
+    key = key.toUpperCase().split("").map((item) => alphabet.indexOf(item));
+    // prettier-ignore
+    let strTemp = str.toUpperCase().split("");
+    strTemp = strTemp.map((item) => alphabet.indexOf(item));
+    let result = [];
+    let check = [];
+    for (let i = 0, j = 0; i < strTemp.length; i++) {
+      if (j < key.length) {
+        if (strTemp[i] != -1) {
+          //encrypt
+          if (action === true) {
+            result.push(
+              strTemp[i] + key[j] < 26
+                ? alphabet[strTemp[i] + key[j]]
+                : alphabet[strTemp[i] + key[j] - 26]
+            );
+          }
+          //decrypt
+          else {
+            result.push(
+              strTemp[i] - key[j] < 0
+                ? alphabet[strTemp[i] - key[j] + 26]
+                : alphabet[strTemp[i] - key[j]]
+            );
+          }
+        } else {
+          j--;
+          result.push(str[i]);
+        }
+        j++;
+      } else {
+        j = 0;
+        i--;
+      }
+    }
+    return this.bool ? result.join("") : result.reverse().join("");
+  }
 }
 
 module.exports = {
